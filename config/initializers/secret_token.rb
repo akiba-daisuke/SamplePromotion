ea5d589d0579453b7a1a6c893709fc7be6177137
akiba <akiba@aki-Ubuntu.(none)>
2013-09-19 13:59:52 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePromotion::Application.config.secret_token = '27bf90987d4f1687b7860010618346edd46daa052734ee13b579695b64a2f9bccc03f9a15533f3d1c3a8ed47dd28f985ddcceee6830fd4977e2dcab39f68fcfa'
